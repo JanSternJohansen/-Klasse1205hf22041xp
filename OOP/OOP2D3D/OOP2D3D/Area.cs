3cf76eb43f6f7521b2f7de9ac1c1053472f804d4
Jan Stern Johansen - TEC <jaj@tec.dk>
2022-04-27 16:29:22 +0200

    Onsdag 27.4 kl 14

--- OOP/OOP2D3D/OOP2D3D/Area.cs
+++ OOP/OOP2D3D/OOP2D3D/Area.cs
@@ -5,12 +5,34 @@ using System;
 namespace OOP2D3D
 {
     public class Area
-    {   
+    {
         // Fields
         private string _name;
         private int _length;
         private int _width;
 
+        public Area() { }
+
+        public Area(string name, int len, int wid)
+        {
+            this.setName(name);
+            this.Length = len;
+            this.Width = wid;   
+        }
+
+        // Properties
+        public int Length 
+        {
+            get {return this._length; }
+            set {this._length = value;} 
+        }
+        public int Width
+        {
+            get { return this._width; }
+            set { this._width = value; }
+        }
+
+        // Metoder
         public void setName(String n)
         {
             if (n.Length > 2 && n.Length < 50)
@@ -25,6 +47,11 @@ namespace OOP2D3D
         public string getName()
         {
             return this._name;
+        }
+
+        public override string ToString()
+        {
+            return $"{ this.getName()}  L: { this.Length}  W: { this.Width}";
         }
 
 
--- OOP/OOP2D3D/OOP2D3D/Program.cs
+++ OOP/OOP2D3D/OOP2D3D/Program.cs
@@ -15,8 +15,25 @@ namespace OOP2D3D
             //a1.length = 7;
             //a1.name = "Torben";
 
-            a1.setName("BooooooooooooooooooooooooooooooooooooBooooooooooooooooooooooooooooooooooo");
+            a1.setName("BoBo");
             Console.WriteLine(a1.getName());
+
+            a1.Length = 20;
+            int len = a1.Length;
+            a1.Width = 12;
+            Console.WriteLine($"{ a1.getName()}  L: { a1.Length}  W: { a1.Width}");
+
+            Console.WriteLine(a1.ToString());
+
+            Area a2 = new Area();
+            a2.setName("Oliver");
+            a2.Length = 34;
+            a2.Width = 13;
+            Console.WriteLine(a2);
+
+            Area a3 = new Area("Subarna", 16, 7);
+            Console.WriteLine(a3);
+
 
             Console.ReadLine();
 
