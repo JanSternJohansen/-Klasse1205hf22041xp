5e2d2c8ae5c1c260546ddded72d132a8da7dd2c7
JanSternJohansen <jaj@tec.dk>
2022-04-27 10:04:50 +0200

    Onsdag 27.4 kl. 10

--- OOP/OOP2D3D/OOP2D3D/Area.cs
+++ OOP/OOP2D3D/OOP2D3D/Area.cs
@@ -5,10 +5,28 @@ using System;
 namespace OOP2D3D
 {
     public class Area
-    {
-        private String name;
-        public int length;
-        public int width;
+    {   
+        // Fields
+        private string _name;
+        private int _length;
+        private int _width;
+
+        public void setName(String n)
+        {
+            if (n.Length > 2 && n.Length < 50)
+            {
+                this._name = n;
+            }
+            else
+            {
+                this._name = "N/A";
+            }
+        }
+        public string getName()
+        {
+            return this._name;
+        }
+
 
     }
 }
--- OOP/OOP2D3D/OOP2D3D/Program.cs
+++ OOP/OOP2D3D/OOP2D3D/Program.cs
@@ -11,10 +11,14 @@ namespace OOP2D3D
         static void Main(string[] args)
         {
             Area a1 = new Area();
-            a1.width = 20;
-            a1.length = 7;
-            a1.name = "Torben";
+            //a1.width = -20;
+            //a1.length = 7;
+            //a1.name = "Torben";
 
+            a1.setName("BooooooooooooooooooooooooooooooooooooBooooooooooooooooooooooooooooooooooo");
+            Console.WriteLine(a1.getName());
+
+            Console.ReadLine();
 
         }
     }
