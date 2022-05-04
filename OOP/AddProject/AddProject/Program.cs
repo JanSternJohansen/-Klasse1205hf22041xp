e58f5bee866a3b8fdb4ab700b4a0b55e560fa6a8
JanSternJohansen <jaj@tec.dk>
2022-05-04 14:15:00 +0200

    Commit onsdag 4.maj kl. 14

---
+++ OOP/AddProject/AddProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Punkt p1 = new Punkt(3, 7);
+            Punkt p2 = new Punkt(6, 5);
+      
+            Punkt p3 = p1.Add(p2);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            
+            Console.WriteLine();
+            Console.WriteLine("P1: " + p1);
+
+            Punkt p4 = p1;
+            Console.WriteLine("P4: " + p4);
+
+            p1.X = 44;
+
+            Console.WriteLine("P1: " + p1);
+            Console.WriteLine("P4: " + p4);
+            //////////////////////////////////////
+            ///
+            p4 = (Punkt)p1.Clone();
+            Console.WriteLine("\nP1: " + p1);
+            Console.WriteLine("P4: " + p4);
+            p4.X = 7;
+            p4.Y = 13;
+            Console.WriteLine("P1: " + p1);
+            Console.WriteLine("P4: " + p4);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP/AddProject/AddProject/Punkt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddProject
+{
+    public class Punkt : IAddable, ICloneable
+    {
+        private int _x;
+        private int _y;
+
+        public int X 
+        { 
+            get { return _x; } 
+            set { this._x = value; } 
+        }
+        public int Y { get { return _y; } set { this._y = value; } }
+
+        public Punkt() { }
+        public Punkt(int x, int y) { this.X = x; this.Y = y; }
+
+        public override string ToString()
+        {
+            return string.Format("X: {0}  Y: {1}", X, Y);
+        }
+
+        public Punkt Add(Punkt p)
+        {
+            Punkt pNew = new Punkt();
+            pNew.X = this.X + p.X;
+            pNew.Y = this.Y + p.Y;
+
+            return pNew;
+        }
+
+        public object Clone()
+        {
+            Punkt newP = new Punkt();
+            newP.X = this.X;
+            newP.Y = this.Y;
+            return newP;
+        }
+    }
+}
