a91dba8da668b6c13459a61a1d013c36559ddec1
JanSternJohansen <jaj@tec.dk>
2022-05-05 10:44:38 +0200

    Commit torsdag 5.5.22

---
+++ OOP/TestStatic/TestStatic/Compute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStatic
+{
+    public class Compute
+    {
+        private int tal1;
+        private int tal2;
+        public static int numberOfObjects;
+        public int MyId;
+        public Compute(int t1, int t2)
+        {
+            numberOfObjects++;
+            MyId = numberOfObjects;
+            this.tal1 = t1;
+            this.tal2 = t2;
+        }
+        public int GetSum()
+        {
+            //Fra non-static kan static kaldes
+            //int tal = GetSum(3, 4);
+            return this.tal1 + this.tal2;
+        }
+        public static int GetSum(int t1, int t2)
+        {
+            // Fra static kan non-static ikke kaldes
+            //GetSum(); // error
+            return t1 + t2;  
+        }
+        public override string ToString()
+        {
+            return "MyId: " + MyId;
+        }
+    }
+}
---
+++ OOP/TestStatic/TestStatic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Compute c1 = new Compute(12, 45);
+            Console.WriteLine("Antal objekter: " + Compute.numberOfObjects);
+            Compute c2 = new Compute(11, 5);
+            Console.WriteLine("Antal objekter: " + Compute.numberOfObjects);
+            Compute c3 = new Compute(2, 22);
+            Console.WriteLine("Antal objekter: " + Compute.numberOfObjects);
+            Compute c4 = new Compute(112, 55);
+            Console.WriteLine("Antal objekter: " + Compute.numberOfObjects);
+
+            Console.WriteLine("12 + 45 = " + c4.GetSum());
+            Console.WriteLine("12 + 45 = " + Compute.GetSum(12,45));
+
+            Compute c5 = new Compute(34, 34);
+            Console.WriteLine("Antal objekter: " + Compute.numberOfObjects);
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+            Console.WriteLine(c5);
+
+            Console.ReadLine();
+
+        }
+    }
+}
